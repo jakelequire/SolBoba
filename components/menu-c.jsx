4338f246b40f9cdbafa8231a2629ec3b845f89e2
Phock <jakelequire@gmail.com>
2022-11-27 14:37:55 -0800

    ###

--- components/menu-c.jsx
+++ components/menu-c.jsx
@@ -16,23 +16,23 @@ function MenuC() {
                  </div>
              
                  <div className="mt-one">
-                     <Image className='mt-img' src={drink.boba_milktea} />
+                     <Image className='mt-img' src={drink.boba_milktea} alt="Classic Milk Tea"/>
                      <h3> Classic Milk Tea </h3>
                  </div>
                  <div className="mt-two">
-                     <Image className='mt-img' src={drink.boba_milktea} />
+                     <Image className='mt-img' src={drink.boba_milktea} alt="Barley Milk Tea"/>
                      <h3> Barley Milk Tea </h3>
                  </div>
                  <div className="mt-three">
-                     <Image className='mt-img' src={drink.honeydew} />
+                     <Image className='mt-img' src={drink.honeydew} alt="Honeydew Milk Tea"/>
                      <h3> Honeydew Milk Tea </h3>
                  </div>
                  <div className="mt-four">
-                     <Image className='mt-img'src={drink.boba_milktea}/>
+                     <Image className='mt-img'src={drink.boba_milktea} alt="Wintermelon Milk Tea"/>
                      <h3> Wintermelon Milk Tea </h3>
                  </div>
                  <div className="mt-five">
-                     <Image className='mt-img'src={drink.thai_tea}/>
+                     <Image className='mt-img'src={drink.thai_tea} alt="Thai Milk Tea"/>
                      <h3> Thai Milk Tea </h3>
                  </div>
                  {/* Specialty Menu */}
@@ -41,47 +41,47 @@ function MenuC() {
                  </div>
 
                   <div className="st-one">
-                     <Image className='mt-img' src={drink.taro_oreo} />
+                     <Image className='mt-img' src={drink.taro_oreo} alt="Thai Milk Tea" />
                      <h3> Taro Oreo Slush </h3>
                  </div>
                  <div className="st-two">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Asian Pear Slush"/>
                      <h3> Asian Pear Slush </h3>
                  </div>
                  <div className="st-three">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Dalgona Latte"/>
                      <h3> Dalgona Latte </h3>
                  </div>
                  <div className="st-four">
-                     <Image className='mt-img'src={drink.tiger_boba}/>
+                     <Image className='mt-img'src={drink.tiger_boba} alt="Brown Sugar Latte"/>
                      <h3> Brown Sugar Latte </h3>
                  </div>
                  <div className="st-five">
-                     <Image className='mt-img'src={drink.tiramisu}/>
+                     <Image className='mt-img'src={drink.tiramisu} alt="Tiramisu Latte"/>
                      <h3> Tiramisu Latte </h3>
                  </div>
                  <div className="st-six">
-                     <Image className='mt-img' src={drink.strawb_milk} />
+                     <Image className='mt-img' src={drink.strawb_milk} alt="Strawberry Milk" />
                      <h3> Strawberry Milk </h3>
                  </div>
                  <div className="st-seven">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Blueberry Milk"/>
                      <h3> Blueberry Milk </h3>
                  </div>
                  <div className="st-eight">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Ube Latte"/>
                      <h3> Ube Latte </h3>
                  </div>
                  <div className="st-nine">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Brown Sugar Oreo"/>
                      <h3> Brown Sugar Oreo </h3>
                  </div>
                  <div className="st-ten">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Dirty Ube Latte"/>
                      <h3> Dirty Ube Latte </h3>
                  </div>
                   <div className="st-eleven">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Injeolmi Latte"/>
                      <h3> Injeolmi Latte </h3>
                  </div>
              </div>
@@ -94,19 +94,19 @@ function MenuC() {
                  </div>
 
                  <div className="mt2-one">
-                     <Image className='mt-img' src={drink.matcha_latte} />
+                     <Image className='mt-img' src={drink.matcha_latte} alt="Matcha Latte"/>
                      <h3> Matcha Latte </h3>
                  </div>
                  <div className="mt2-two">
-                     <Image className='mt-img' src={drink.strawb_matcha} />
+                     <Image className='mt-img' src={drink.strawb_matcha} alt="Strawberry Matcha"/>
                      <h3> Strawberry Matcha </h3>
                  </div>
                  <div className="mt2-three">
-                     <Image className='mt-img' src={drink.blueb_matcha} />
+                     <Image className='mt-img' src={drink.blueb_matcha} alt="Blueberry Matcha"/>
                      <h3> Blueberry Matcha </h3>
                  </div>
                  <div className="mt2-four">
-                     <Image className='mt-img' src={drink.dirty_matcha} />
+                     <Image className='mt-img' src={drink.dirty_matcha} alt="Dirty Matcha"/>
                      <h3> Dirty Matcha </h3>
                  </div>
                  {/* Fruit Tea Menu */}
@@ -115,35 +115,35 @@ function MenuC() {
                  </div>
 
                   <div className="ft-one">
-                     <Image className='mt-img' src={drink.lychee_hibiscus} />
+                     <Image className='mt-img' src={drink.lychee_hibiscus} alt="Lychee Hibiscus Tea"/>
                      <h3> Lychee Hibiscus Tea </h3>
                  </div>
                  <div className="ft-two">
-                     <Image className='mt-img' src={drink.hibiscus_mango} />
+                     <Image className='mt-img' src={drink.hibiscus_mango} alt="Mango Hibiscus tea" />
                      <h3> Mango Hibiscus Tea </h3>
                  </div>
                  <div className="ft-three">
-                     <Image className='mt-img' src={drink.orange_guava} />
+                     <Image className='mt-img' src={drink.orange_guava} alt="Pog Green Tea"/>
                      <h3> Pog Green Tea </h3>
                  </div>
                  <div className="ft-four">
-                     <Image className='mt-img'src={drink.bloopi}/>
+                     <Image className='mt-img'src={drink.bloopi} alt="Bloopi"/>
                      <h3> Bloopi </h3>
                  </div>
                  <div className="ft-five">
-                     <Image className='mt-img'src={drink.strawb_blacktea}/>
+                     <Image className='mt-img'src={drink.strawb_blacktea} alt="Strawberry Black Tea"/>
                      <h3> Strawberry Black Tea</h3>
                  </div>
                  <div className="ft-six">
-                     <Image className='mt-img' src={drink.lychee_hibiscus} />
+                     <Image className='mt-img' src={drink.lychee_hibiscus} alt="Strawberry Green Tea"/>
                      <h3> Strawberry Green Tea </h3>
                  </div>
                  <div className="ft-seven">
-                     <Image className='mt-img' src={drink.hibiscus_mango} />
+                     <Image className='mt-img' src={drink.hibiscus_mango} alt="Purple Flurp"/>
                      <h3> Purple Flurp </h3>
                  </div>
                  <div className="ft-eight">
-                     <Image className='mt-img' src={drink.placeholder} />
+                     <Image className='mt-img' src={drink.placeholder} alt="Lemon Thai Tea" />
                      <h3> Lemon Thai Tea </h3>
                  </div>
                  {/* Extras Container - Second Menu */}
@@ -166,11 +166,11 @@ function MenuC() {
                  </div>
                   <div className="ext-three">
                      <h2 className="il-header">Ice Level</h2>
-                     <Image src={drink.icelevel} alt="" />
+                     <Image src={drink.icelevel} alt="Ice Level" />
                  </div>
                   <div className="ext-four">
                      <h2 className="sl-header">Sugar Level</h2>
-                     <Image src={drink.sugarlevel} alt="" />
+                     <Image src={drink.sugarlevel} alt="Sugar Level" />
                  </div>
              </div>
 
--- pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import menu from '../../public/media/SolBoba-Menu.pdf'
-//export default function handler(req, res) {
-//  res.status(200).json({ name: 'John Doe' })
-//}
-
-export default function downloadMenu(req, res) {
-    res.status(200).json({ content: menu })
-};
