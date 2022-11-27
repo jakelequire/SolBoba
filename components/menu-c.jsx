3e5719597954da5ec0b22cb385dafe7abb83ebdf
Phock <jakelequire@gmail.com>
2022-11-26 18:37:25 -0800

    ###

--- components/menu-c.jsx
+++ components/menu-c.jsx
@@ -1,33 +1,5 @@
 import Image from 'next/image'
-
-
-
-
-import placeholder from '../public/media/drinks/placeholder.png'
-import banana_milk from '../public/media/drinks/banana-milk.png'
-import bloopi from '../public/media/drinks/bloopi.png'
-import blueb_matcha from '../public/media/drinks/blueb-matcha.png'
-import boba_milktea from '../public/media/drinks/boba-milktea.png'
-import dirty_matcha from '../public/media/drinks/dirty-matcha.png'
-import gfade from '../public/media/drinks/gfade.png'
-import hibiscus_mango from '../public/media/drinks/hibiscus-mango.png'
-import lemonade from '../public/media/drinks/lemonade.png'
-import lychee_hibiscus from '../public/media/drinks/lychee-hibiscus.png'
-import matcha_latte from '../public/media/drinks/matcha-latte.png'
-import matcha_oreo from '../public/media/drinks/matcha-oreo.png'
-import orange_guava from '../public/media/drinks/orange-guava.png'
-import purple_flurp from '../public/media/drinks/purple-flurp.png'
-import snickerdoodle from '../public/media/drinks/snickerdoodle.png'
-import strawb_blacktea from '../public/media/drinks/strawb-blacktea.png'
-import strawb_matcha from '../public/media/drinks/strawb-matcha.png'
-import strawb_milk from '../public/media/drinks/strawb-milk.png'
-import taro_oreo from '../public/media/drinks/taro-oreo.png'
-import thai_tea from '../public/media/drinks/thai-tea.png'
-import tiger_boba from '../public/media/drinks/tiger-boba.png'
-import tiramisu from '../public/media/drinks/tiramisu.png'
-import honeydew from '../public/media/drinks/honeydew.png'
-import icelevel from '../public/media/icelevel.png'
-import sugarlevel from '../public/media/sugarlevel.png'
+import drink from '../pages/api/_media'
 
 function MenuC() {
 
@@ -44,23 +16,23 @@ function MenuC() {
                     </div>
                 
                     <div className="mt-one">
-                        <Image className='mt-img' src={boba_milktea} />
+                        <Image className='mt-img' src={drink.boba_milktea} />
                         <h3> Classic Milk Tea </h3>
                     </div>
                     <div className="mt-two">
-                        <Image className='mt-img' src={boba_milktea} />
+                        <Image className='mt-img' src={drink.boba_milktea} />
                         <h3> Barley Milk Tea </h3>
                     </div>
                     <div className="mt-three">
-                        <Image className='mt-img' src={honeydew} />
+                        <Image className='mt-img' src={drink.honeydew} />
                         <h3> Honeydew Milk Tea </h3>
                     </div>
                     <div className="mt-four">
-                        <Image className='mt-img'src={boba_milktea}/>
+                        <Image className='mt-img'src={drink.boba_milktea}/>
                         <h3> Wintermelon Milk Tea </h3>
                     </div>
                     <div className="mt-five">
-                        <Image className='mt-img'src={thai_tea}/>
+                        <Image className='mt-img'src={drink.thai_tea}/>
                         <h3> Thai Milk Tea </h3>
                     </div>
                     {/* Specialty Menu */}
@@ -69,47 +41,47 @@ function MenuC() {
                     </div>
 
                      <div className="st-one">
-                        <Image className='mt-img' src={taro_oreo} />
+                        <Image className='mt-img' src={drink.taro_oreo} />
                         <h3> Taro Oreo Slush </h3>
                     </div>
                     <div className="st-two">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Asian Pear Slush </h3>
                     </div>
                     <div className="st-three">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Dalgona Latte </h3>
                     </div>
                     <div className="st-four">
-                        <Image className='mt-img'src={tiger_boba}/>
+                        <Image className='mt-img'src={drink.tiger_boba}/>
                         <h3> Brown Sugar Latte </h3>
                     </div>
                     <div className="st-five">
-                        <Image className='mt-img'src={tiramisu}/>
+                        <Image className='mt-img'src={drink.tiramisu}/>
                         <h3> Tiramisu Latte </h3>
                     </div>
                     <div className="st-six">
-                        <Image className='mt-img' src={strawb_milk} />
+                        <Image className='mt-img' src={drink.strawb_milk} />
                         <h3> Strawberry Milk </h3>
                     </div>
                     <div className="st-seven">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Blueberry Milk </h3>
                     </div>
                     <div className="st-eight">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Ube Latte </h3>
                     </div>
                     <div className="st-nine">
-                        <Image className='mt-img' src={tiger_boba} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Brown Sugar Oreo </h3>
                     </div>
                     <div className="st-ten">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Dirty Ube Latte </h3>
                     </div>
                      <div className="st-eleven">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Injeolmi Latte </h3>
                     </div>
 
@@ -124,19 +96,19 @@ function MenuC() {
                     </div>
 
                     <div className="mt2-one">
-                        <Image className='mt-img' src={matcha_latte} />
+                        <Image className='mt-img' src={drink.matcha_latte} />
                         <h3> Matcha Latte </h3>
                     </div>
                     <div className="mt2-two">
-                        <Image className='mt-img' src={strawb_matcha} />
+                        <Image className='mt-img' src={drink.strawb_matcha} />
                         <h3> Strawberry Matcha </h3>
                     </div>
                     <div className="mt2-three">
-                        <Image className='mt-img' src={blueb_matcha} />
+                        <Image className='mt-img' src={drink.blueb_matcha} />
                         <h3> Blueberry Matcha </h3>
                     </div>
                     <div className="mt2-four">
-                        <Image className='mt-img' src={dirty_matcha} />
+                        <Image className='mt-img' src={drink.dirty_matcha} />
                         <h3> Dirty Matcha </h3>
                     </div>
                     {/* Fruit Tea Menu */}
@@ -144,35 +116,35 @@ function MenuC() {
                         <h1 className="menu-header"> Fruit Tea </h1>
                     </div>
                      <div className="ft-one">
-                        <Image className='mt-img' src={lychee_hibiscus} />
+                        <Image className='mt-img' src={drink.lychee_hibiscus} />
                         <h3> Lychee Hibiscus Tea </h3>
                     </div>
                     <div className="ft-two">
-                        <Image className='mt-img' src={hibiscus_mango} />
+                        <Image className='mt-img' src={drink.hibiscus_mango} />
                         <h3> Mango Hibiscus Tea </h3>
                     </div>
                     <div className="ft-three">
-                        <Image className='mt-img' src={orange_guava} />
+                        <Image className='mt-img' src={drink.orange_guava} />
                         <h3> Pog Green Tea </h3>
                     </div>
                     <div className="ft-four">
-                        <Image className='mt-img'src={bloopi}/>
+                        <Image className='mt-img'src={drink.bloopi}/>
                         <h3> Bloopi </h3>
                     </div>
                     <div className="ft-five">
-                        <Image className='mt-img'src={strawb_blacktea}/>
+                        <Image className='mt-img'src={drink.strawb_blacktea}/>
                         <h3> Strawberry Black Tea</h3>
                     </div>
                     <div className="ft-six">
-                        <Image className='mt-img' src={lychee_hibiscus} />
+                        <Image className='mt-img' src={drink.lychee_hibiscus} />
                         <h3> Strawberry Green Tea </h3>
                     </div>
                     <div className="ft-seven">
-                        <Image className='mt-img' src={hibiscus_mango} />
+                        <Image className='mt-img' src={drink.hibiscus_mango} />
                         <h3> Purple Flurp </h3>
                     </div>
                     <div className="ft-eight">
-                        <Image className='mt-img' src={placeholder} />
+                        <Image className='mt-img' src={drink.placeholder} />
                         <h3> Lemon Thai Tea </h3>
                     </div>
                     <div className="ext-header">
@@ -193,11 +165,11 @@ function MenuC() {
                     </div>
                      <div className="ext-three">
                         <h2 className="il-header">Ice Level</h2>
-                        <Image src={icelevel} alt="" />
+                        <Image src={drink.icelevel} alt="" />
                     </div>
                      <div className="ext-four">
                         <h2 className="sl-header">Sugar Level</h2>
-                        <Image src={sugarlevel} alt="" />
+                        <Image src={drink.sugarlevel} alt="" />
                     </div>
             </div>
             <div className="download-container"> 
---
+++ pages/api/_media.js
@@ -0,0 +1,57 @@
+import placeholder from '../../public/media/drinks/placeholder.png'
+import banana_milk from '../../public/media/drinks/banana-milk.png'
+import bloopi from '../../public/media/drinks/bloopi.png'
+import blueb_matcha from '../../public/media/drinks/blueb-matcha.png'
+import boba_milktea from '../../public/media/drinks/boba-milktea.png'
+import dirty_matcha from '../../public/media/drinks/dirty-matcha.png'
+import gfade from '../../public/media/drinks/gfade.png'
+import hibiscus_mango from '../../public/media/drinks/hibiscus-mango.png'
+import lemonade from '../../public/media/drinks/lemonade.png'
+import lychee_hibiscus from '../../public/media/drinks/lychee-hibiscus.png'
+import matcha_latte from '../../public/media/drinks/matcha-latte.png'
+import matcha_oreo from '../../public/media/drinks/matcha-oreo.png'
+import orange_guava from '../../public/media/drinks/orange-guava.png'
+import purple_flurp from '../../public/media/drinks/purple-flurp.png'
+import snickerdoodle from '../../public/media/drinks/snickerdoodle.png'
+import strawb_blacktea from '../../public/media/drinks/strawb-blacktea.png'
+import strawb_matcha from '../../public/media/drinks/strawb-matcha.png'
+import strawb_milk from '../../public/media/drinks/strawb-milk.png'
+import taro_oreo from '../../public/media/drinks/taro-oreo.png'
+import thai_tea from '../../public/media/drinks/thai-tea.png'
+import tiger_boba from '../../public/media/drinks/tiger-boba.png'
+import tiramisu from '../../public/media/drinks/tiramisu.png'
+import honeydew from '../../public/media/drinks/honeydew.png'
+import icelevel from '../../public/media/icelevel.png'
+import sugarlevel from '../../public/media/sugarlevel.png'
+
+
+const drink = {
+    placeholder: placeholder,
+    banana_milk: banana_milk,
+    bloopi: bloopi,
+    blueb_matcha: blueb_matcha,
+    boba_milktea: boba_milktea,
+    dirty_matcha: dirty_matcha,
+    gfade: gfade,
+    hibiscus_mango: hibiscus_mango,
+    lemonade: lemonade,
+    lychee_hibiscus: lychee_hibiscus,
+    matcha_latte: matcha_latte,
+    matcha_oreo: matcha_oreo,
+    orange_guava: orange_guava,
+    purple_flurp: purple_flurp,
+    snickerdoodle: snickerdoodle,
+    strawb_blacktea: strawb_blacktea,
+    strawb_matcha: strawb_matcha,
+    strawb_milk: strawb_milk,
+    taro_oreo: taro_oreo,
+    thai_tea: thai_tea,
+    tiger_boba: tiger_boba,
+    tiramisu: tiramisu,
+    honeydew: honeydew,
+    icelevel: icelevel,
+    sugarlevel: sugarlevel
+}
+
+
+export default drink;
