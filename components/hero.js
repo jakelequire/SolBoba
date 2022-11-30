08a7e5813c80c137915db88eef3261a954a6bebc
Phock <jakelequire@gmail.com>
2022-11-30 07:58:41 -0800

    ###

--- components/hero.js
+++ components/hero.js
@@ -18,29 +18,30 @@ function Hero() {
         <div className="Container">
             {/* Navbar */}
             <Navbar className="navbar" bg="dark" variant="dark">
-                <Container>
-                    <Nav id="nav-group" >
-                        <Nav.Link id="nav-link" href="/menu">Menu</Nav.Link>
-                        <Nav.Link id="nav-link" href={link.yelp}>Reviews</Nav.Link>
-                        <Nav.Link id="nav-order" href={link.uberEats}>ORDER ONLINE</Nav.Link>
-                        {/* Sol Boba Logo*/}
-                        <Navbar.Brand className="navbar-brand" href="/">
-                            <div className="logo-image">
-                                <Image className="img-fluid" src={icon.logo} alt="" />
-                            </div>
-                        </Navbar.Brand>
-                        {/* Brand Icons */}
-                        <Navbar.Brand className="navbar-icons">
-                            <div className="icon-image">
-                                <a href={link.facebook}> <Image className="fb-logo" src={icon.facebook} alt="" /></a>
-                                <a href={link.instagram}> <Image className="insta-logo" src={icon.instagram} alt="" /></a>
-                                <a href={link.yelp}> <Image className="yelp-logo" src={icon.yelp} alt="" /></a>
-                            </div>
-                        </Navbar.Brand>
-                    </Nav>
-                </Container>
-            </Navbar>
 
+                    <Nav id="nav-group" >
+                    {/* Brand Icons */}
+                    <Container className="nav-icon-container"> 
+                        <Nav.Link id="nav-icon" className="nav-icon" href={link.facebook}><Image className="fb-logo" src={icon.facebook} alt="" /></Nav.Link>
+                        <Nav.Link id="nav-icon" className="nav-icon" href={link.instagram}><Image className="insta-logo" src={icon.instagram} alt="" /></Nav.Link>
+                        <Nav.Link id="nav-icon" className="nav-icon" href={link.yelp}><Image className="yelp-logo" src={icon.yelp} alt="" /></Nav.Link>
+                    </Container>
+                    {/* Logo */}
+                    <Container className="nav-logo-container">
+                        <Nav.Link className="logo-image" >
+                                <Image className="img-fluid" src={icon.logo} alt="" />
+                        </Nav.Link>
+                    </Container>
+                    {/* Site Links */}
+                    <Container className="nav-links-container">
+                        <Nav.Link id="nav-link" className="nav-link" href="/menu">Menu</Nav.Link>
+                        <Nav.Link id="nav-link" className="nav-link" href={link.yelp}>Reviews</Nav.Link>
+                        <Nav.Link id="nav-order" className="nav-link" href={link.uberEats}>ORDER ONLINE</Nav.Link>
+                    </Container>
+                    </Nav>
+
+            </Navbar>
+            
             {/* Hero */}
             <div className="hero-wrapper">
                 <div className="hero-img">
@@ -52,3 +53,64 @@ function Hero() {
 }
 
 export default Hero;
+
+/*
+    q: Why can't I move the items around in the nav-group container?
+    a: The items are in a flexbox container, and the order of the items is determined by the order of the items in the HTML.
+    q: how do I position nav-icon and nav-link?
+    a: use flexbox to position the items in the container
+    q: what properties do I need to use to position the items?
+    a: use flexbox to position the items in the container
+    q: what is the code for flexbox?
+    a: display: flex;
+    q: what is the name of the property i need to use display: flex;
+    a: flex-direction
+    q: what are the values for flex-direction?
+    a: row, column, row-reverse, column-reverse
+    q: what is the default value for flex-direction?
+    a: row
+    q: what is the id that I need to use flexbox for the nav-group container?
+    a: nav-group
+    q: i changed nav-group to flexbox and i cant change the position of the children
+    a: you need to use flexbox properties to position the children
+    q: what properties do I need to use?
+    a: justify-content, align-items, align-content
+    q: what are the values for justify-content?
+    a: flex-start, flex-end, center, space-between, space-around, space-evenly
+    q: what are the values for align-items?
+    a: flex-start, flex-end, center, baseline, stretch
+    q: what are the values for align-content?
+    a: flex-start, flex-end, center, space-between, space-around, stretch
+    q: what do I need to do to position the nav-icons to the right and nav-link to the left?
+    a: use justify-content: flex-end and justify-content: flex-start
+    q: I want to position the nav-icons to the right and nav-link to the left, but I want the nav-link to be centered vertically
+    a: use justify-content: flex-end and justify-content: flex-start and align-items: center
+    q: I turned the nav-group into a flexbox container, but the items wont reposition with justify-content: flex-end and justify-content: flex-start and align-items: center
+    a: you need to use flexbox properties to position the children
+    q: what properties do I need to use?
+    a: justify-content, align-items, align-content
+    q: I used the flexbox properties, but the items wont reposition with justify-content: flex-end and justify-content: flex-start and align-items: center
+    a: you need to use flexbox properties to position the children
+    q: which child do I need to use flexbox properties on?
+    a: the children of the flexbox container
+    q: the <Container> or <Nav.Link>?
+    a: the <Nav.Link>
+    q: I changed the flexbox properties of the children, <Nav.Link>, and it still isn't repositioning
+    a: you need to use flexbox properties to position the children
+    q: what is the HTML code for the flexbox properties?
+    a: display: flex;
+    q: what is the name of the child that needs to be flexbox?
+    a: nav-icon
+    q: nav-icon is a flexbox, is it not?
+    a: no, it is not
+    q: when you say nav-icon, are you talking about the <Nav.Link>?
+    a: yes
+    q: I changed the <Nav.Link> to flexbox, but it still isn't repositioning
+    a: you need to use flexbox properties to position the children
+    q: when you say nav-icon, does it matter if I use the className or the Id?
+    a: no, it does not
+    q: I put .nav-icon {display: flex;} in the CSS, but it still isn't repositioning
+    a: you need to use flexbox properties to position the children
+    q: it's written in the css that it is a flex
+    a: you need to use flexbox properties to position the children
+*/
--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
+///** @type {import('next').NextConfig} */
+//const nextConfig = {
+//    reactStrictMode: true,
     
-}
+//}
 
 module.exports =  {
   reactStrictMode: true,
