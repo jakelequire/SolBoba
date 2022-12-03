7445575ba855c1d1e8fea410f3bc743c0877b108
Phock <jakelequire@gmail.com>
2022-12-02 21:59:39 -0800

    ###

--- components/hero.js
+++ components/hero.js
@@ -21,24 +21,30 @@ function Hero() {
 
                     <Nav id="nav-group" >
                     {/* Brand Icons */}
-                    <Container className="nav-icon-container"> 
-                        <Nav.Link id="nav-icon" className="nav-icon" href={link.facebook}><Image className="fb-logo" src={icon.facebook} alt="" /></Nav.Link>
-                        <Nav.Link id="nav-icon" className="nav-icon" href={link.instagram}><Image className="insta-logo" src={icon.instagram} alt="" /></Nav.Link>
-                        <Nav.Link id="nav-icon" className="nav-icon" href={link.yelp}><Image className="yelp-logo" src={icon.yelp} alt="" /></Nav.Link>
-                    </Container>
+            <Container className="nav-icon-container"> 
+                <Nav.Link id="nav-icon" className="nav-icon" href={link.facebook}>
+                    <Image className="fb-logo" src={icon.facebook} alt="" />
+                </Nav.Link>
+                <Nav.Link id="nav-icon" className="nav-icon" href={link.instagram}>
+                    <Image className="insta-logo" src={icon.instagram} alt="" />
+                </Nav.Link>
+                <Nav.Link id="nav-icon" className="nav-icon" href={link.yelp}>
+                    <Image className="yelp-logo" src={icon.yelp} alt="" />
+                </Nav.Link>
+            </Container>
                     {/* Logo */}
-                    <Container className="nav-logo-container">
-                        <Nav.Link className="logo-image" href="/">
-                                <Image className="img-fluid" src={icon.logo} alt="" />
-                        </Nav.Link>
-                    </Container>
+            <Container className="nav-logo-container">
+                <Nav.Link className="logo-image" href="/">
+                    <Image className="img-fluid" src={icon.logo} alt="" />
+                </Nav.Link>
+            </Container>
                     {/* Site Links */}
-                    <Container className="nav-links-container">
-                        <Nav.Link id="nav-link" className="nav-link" href="/menu">Menu</Nav.Link>
-                        <Nav.Link id="nav-link" className="nav-link" href={link.yelp}>Reviews</Nav.Link>
-                        <Nav.Link id="nav-order" className="nav-link" href={link.uberEats}>ORDER ONLINE</Nav.Link>
-                    </Container>
-                    </Nav>
+            <Container className="nav-links-container">
+                <Nav.Link id="nav-link" className="nav-link" href="/menu">Menu</Nav.Link>
+                <Nav.Link id="nav-link" className="nav-link" href={link.yelp}>Reviews</Nav.Link>
+                <Nav.Link id="nav-order" className="nav-link" href={link.uberEats}>ORDER ONLINE</Nav.Link>
+            </Container>
+            </Nav>
 
             </Navbar>
             
